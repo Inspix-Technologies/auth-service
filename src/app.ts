a5b162d1b20ef3b4cfa0785fc30c2975e3758664
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-07-04 16:58:45 +0800

    feature/add get user data controller

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,9 @@ import User from './UserModel';
 import { auth } from 'firebase-admin';
 import mustAuthorized from './middlewares/auth-middleware';
 import EndUserError from './errors/EndUserError';
+import TokenError from './errors/TokenError';
+import tokenAuthenticator from './token-authenticator';
+import AuthError from './errors/AuthError';
 
 declare global {
   namespace Express {
@@ -18,8 +21,28 @@ declare global {
 
 const app = express();
 sequelize.addModels([User]);
-app.use(cors());
+app.use(cors({origin: "http://localhost:3000", credentials: true}));
 app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const idToken: string | undefined = req.headers.authorization?.split(' ')[1]
+  console.log('hi')
+  if (!idToken) 
+    return TokenError
+      .createClientError(1, 400, "idtoken not found", [{name: 'idToken', message: 'missing'}])
+      .createResponse(res)
+  const decodedToken = await tokenAuthenticator(idToken)
+  if (!decodedToken) 
+    return TokenError
+      .createClientError(2, 403, "unauthorized", [{name: 'idToken', message: 'unauthorized'}])
+      .createResponse(res)
+  const user = await User.findOne({where: {uid: decodedToken.uid}})
+  if (!user)
+    return new AuthError(4, 403, "user data missing", [{name: 'userData', message: 'missing'}])
+      .createResponse(res)
+  
+  res.status(200).json(user.toJSON())
+})
 
 //TODO: make this reusable on other services for validation?
 app.post("/", mustAuthorized, async (req, res, next) => {
