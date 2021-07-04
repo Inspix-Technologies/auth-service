317ea57c4517a9cfea67c8a2c04cf5a775dc0f9e
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-07-04 12:32:07 +0800

    feature/update error handling

--- src/app.ts
+++ src/app.ts
@@ -4,25 +4,40 @@ dotenv.config();
 import cors from 'cors';
 import sequelize from './database/Database';
 import User from './UserModel';
-import tokenAuthenticator from './token-authenticator';
+import { auth } from 'firebase-admin';
+import mustAuthorized from './middlewares/auth-middleware';
+import EndUserError from './errors/EndUserError';
+
+declare global {
+  namespace Express {
+    interface Request {
+      decodedIdToken: auth.DecodedIdToken
+    }
+  }
+}
 
 const app = express();
 sequelize.addModels([User]);
 app.use(cors());
 app.use(express.json());
 
-app.post("/", async (req, res, next) => {
-  const idToken: string = req.body.idToken
-  const name: string = req.body.name
-  if (!idToken) return res.status(400).json('idToken not found')
-  if (!name) return res.status(400).json('name not found')
-  const decodedToken = await tokenAuthenticator(idToken)
-  if (!decodedToken) return res.status(403).json('token unauthorized')
+//TODO: make this reusable on other services for validation?
+app.post("/", mustAuthorized, async (req, res, next) => {
+  let name: string | undefined;
+
+  try {
+    name = req.body['name']
+    if (!name) throw "name missing"
+   } catch (e) {
+     console.error(e)
+     return new EndUserError(3, 422, "name not found", [{name: "name", message: `missing (found ${name}`}]).createResponse(res)
+   }
+  const decodedIdToken = req.decodedIdToken
   User.create({
-    uid: decodedToken.uid,
+    uid: decodedIdToken.uid,
     name: name,
-    email: decodedToken.email!,
-    isVerified: decodedToken.email_verified? true : false
+    email: decodedIdToken.email!,
+    isVerified: decodedIdToken.email_verified? true : false
   })
   res.status(201).json("success")
 })
