72e96796a04e1dce1281e0ca502b0e39e7a18fde
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-08-09 00:25:39 +0800

    feature(security): add allowed headers

--- src/app.ts
+++ src/app.ts
@@ -45,7 +45,7 @@ app.use(async (req, res, next) => {
   );
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
+    'X-Requested-With,content-type,authorization,inspix-api-key'
   );
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
